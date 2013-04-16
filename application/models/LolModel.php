2c17f20293adeab6cf9f3a5f2925f5239abc306d
MrPing <amsellem.jonathan@gmail.com>
2013-04-16 19:46:23 +0200

    model

--- application/models/LolModel.php
+++ application/models/LolModel.php
@@ -3,7 +3,7 @@ class LolModel extends Sally_Model
 {
   public function getEmail()
   {
-    $db = Sally_Db::getConnection();
+    $db = Sally_Db::getConnection('my local db');
     $stmt = $db->prepare('SELECT email FROM users WHERE id = :id LIMIT 1');
     $stmt->execute(array('id' => 1));
     $result = $stmt->fetch();
