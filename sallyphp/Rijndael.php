e4f2dfde1c775a4ab8f163b16d7233515845db77
MrPing <amsellem.jonathan@gmail.com>
2013-10-01 18:12:29 +0200

    v1.131001.1

--- sallyphp/Rijndael.php
+++ sallyphp/Rijndael.php
@@ -7,11 +7,39 @@
  * @license   https://github.com/MrPing/sallyphp#license
  */
 
+namespace sally;
+
+/**
+ * Sally Rijndael
+*/
 class Rijndael
 {
+  /**
+   * @var boolean
+  */
   private $_key = false;
+
+  /**
+   * Rijndael instance
+   * @var mixed
+  */
   protected static $_instance = false;
 
+  /**
+   * Rijndael constructor
+   * @param string key
+  */
+  public function __construct($key = null)
+  {
+    if ($key) {
+      $this->setKey($key);
+    }
+  }
+
+  /**
+   * Rijndael get instance
+   * @return object
+  */
   public static function getInstance()
   {
     if (!self::$_instance) {
@@ -20,6 +48,20 @@ class Rijndael
     return self::$_instance;
   }
 
+  /**
+   * Définir une clef de cryptage
+   * @param string
+  */
+  public function setKey($key)
+  {
+    $this->_key = $key;
+  }
+
+  /**
+   * Encrypt
+   * @param string
+   * @return string
+  */
   public function encrypt($encrypt)
   {
     $iv = mcrypt_create_iv(mcrypt_get_iv_size(MCRYPT_RIJNDAEL_128, MCRYPT_MODE_ECB), MCRYPT_RAND);
@@ -28,17 +70,16 @@ class Rijndael
     return $encode;
   }
 
+  /**
+   * Decrypt
+   * @param string
+   * @return string
+  */
   public function decrypt($decrypt)
   {
     $decoded = base64_decode($decrypt);
     $iv = mcrypt_create_iv(mcrypt_get_iv_size(MCRYPT_RIJNDAEL_128, MCRYPT_MODE_ECB), MCRYPT_RAND);
     $decrypted = trim(mcrypt_decrypt(MCRYPT_RIJNDAEL_128, $this->_key, trim($decoded), MCRYPT_MODE_ECB, $iv));
     return $decrypted;
-  }
-
-  public function setKey($key)
-  {
-    $this->_key = $key;
-    return true;
   }
 }
