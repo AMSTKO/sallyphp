c31b8be8141bc8d472c82bab14796630d6257daa
MrPing <amsellem.jonathan@gmail.com>
2013-10-03 15:37:26 +0200

    v1.131003.1

--- sallyphp/Request.php
+++ sallyphp/Request.php
@@ -14,18 +14,31 @@ namespace sally;
 */
 class Request
 {
+  /**
+   * @var boolean
+  */
   private $_method = false;
   private $_module = false;
   private $_controller = false;
   private $_action = false;
+
+  /**
+   * @var array
+  */
+  private $_segment = array();
   private $_data = array();
 
+  /**
+   * Request constructor
+   * @param string method POST, GET, DELETE, PUT...
+  */
   public function __construct($method = null)
   {
     if ($method) {
-      $this->_method = $method;
+      $this->setMethod($method);
     } else {
-      $this->_method = $_SERVER['REQUEST_METHOD'];
+      $this->setMethod($_SERVER['REQUEST_METHOD']);
+      $this->_data = $_POST;
     }
   }
 
@@ -126,64 +139,121 @@ class Request
     return true;
   }
 
+  /**
+   * set method
+   * @param string method POST, GET, DELETE, PUT...
+  */
   public function setMethod($value)
   {
     $this->_method = $value;
   }
 
+  /**
+   * get method
+   * @return string method POST, GET, DELETE, PUT...
+  */
   public function getMethod()
   {
     return $this->_method;
   }
 
+  /**
+   * set segment
+   * @param string
+   * @param string
+  */
   public function setSegment($name, $value)
+  {
+    $this->_segment[$name] = $value;
+  }
+
+  /**
+   * get segment
+   * @param string
+   * @return string
+  */
+  public function getSegment($name)
+  {
+    if (array_key_exists($name, $this->_segment)) {
+      return $this->_segment[$name];
+    } else {
+      return false;
+    }
+  }
+
+  /**
+   * set data
+   * @param string
+   * @param string
+  */
+  public function setData($name, $value)
   {
     $this->_data[$name] = $value;
   }
 
-  public function getSegment($name)
+  /**
+   * get data
+   * @param string
+   * @return string
+  */
+  public function getData($name)
   {
     if (array_key_exists($name, $this->_data)) {
       return $this->_data[$name];
     } else {
-      return null;
+      return false;
     }
   }
 
-  public function getPost($name)
-  {
-    if (array_key_exists($name, $_POST)) {
-      return $_POST[$name];
-    } else {
-      return null;
-    }
-  }
-
+  /**
+   * set module
+   * @param string
+  */
   public function setModule($name)
   {
     $this->_module = $name;
   }
 
+  /**
+   * set controller
+   * @param string
+  */
   public function setController($name)
   {
     $this->_controller = $name;
   }
 
+  /**
+   * set action
+   * @param string
+  */
   public function setAction($name)
   {
     $this->_action = $name;
   }
 
+  /**
+   * get module
+   * @return string
+  */
   public function getModule()
   {
     return $this->_module;
   }
 
+  /**
+   * get controller
+   * @return string
+  */
   public function getController()
   {
     return $this->_controller;
   }
 
+  /**
+   * get action
+   * @return string
+  */
   public function getAction()
   {
     return $this->_action;
