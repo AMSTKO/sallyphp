15151d6595fcc7539e4ca6238aa1feef6bda8abf
MrPing <amsellem.jonathan@gmail.com>
2013-10-10 03:22:16 +0200

    license

--- sallyphp/Trafficker.php
+++ sallyphp/Trafficker.php
@@ -3,7 +3,6 @@
  * SallyPHP
  *
  * @link      https://github.com/MrPing/sallyphp
- * @copyright Copyright (c) 2013, Jonathan Amsellem.
  * @license   https://github.com/MrPing/sallyphp#license
 */
 
